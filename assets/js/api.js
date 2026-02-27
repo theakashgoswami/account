a5b28cf326b20954273ca094ea787655242938bd
theakashgoswami <founder@agtechscript.in>
2026-02-27 17:05:28 +0530

    FFFF

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,7 +2,7 @@
    USER NOTIFICATIONS
 =============================== */
 async function getNotifications() {
-    const res = await fetch(`${API_BASE}/api/user/notifications`, {
+    const res = await fetch(`${WORKER_URL}/api/user/notifications`, {
         credentials: "include"
     });
     return await res.json();
@@ -12,7 +12,7 @@ async function getNotifications() {
    USER PURCHASES
 =============================== */
 async function getPurchases() {
-    const res = await fetch(`${API_BASE}/api/user/purchases`, {
+    const res = await fetch(`${WORKER_URL}/api/user/purchases`, {
         credentials: "include"
     });
     return await res.json();
@@ -22,7 +22,7 @@ async function getPurchases() {
    USER EARN HISTORY
 =============================== */
 async function getEarn() {
-    const res = await fetch(`${API_BASE}/api/user/earn`, {
+    const res = await fetch(`${WORKER_URL}/api/user/earn`, {
         credentials: "include"
     });
     return await res.json();
@@ -32,7 +32,7 @@ async function getEarn() {
    USER USE (REDEEM)
 =============================== */
 async function getUse() {
-    const res = await fetch(`${API_BASE}/api/user/use`, {
+    const res = await fetch(`${WORKER_URL}/api/user/use`, {
         credentials: "include"
     });
     return await res.json();
@@ -42,7 +42,7 @@ async function getUse() {
    USER POINTS LOG
 =============================== */
 async function getPointsLog() {
-    const res = await fetch(`${API_BASE}/api/user/points-log`, {
+    const res = await fetch(`${WORKER_URL}/api/user/points-log`, {
         credentials: "include"
     });
     return await res.json();
