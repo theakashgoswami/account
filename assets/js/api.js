d3527765142d2b08cae60b0cbb891461f6d9fe22
theakashgoswami <founder@agtechscript.in>
2026-02-27 17:21:12 +0530

    api

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,11 +3,14 @@
 =============================== */
 async function getNotifications() {
     const res = await fetch(`${CONFIG.WORKER_URL}/api/user/notifications`, {
-        credentials: "include"
+        credentials: "include",
+        headers: {
+            "X-Client-Host": window.location.host
+        }
     });
+
     return await res.json();
 }
-
 /* ===============================
    USER PURCHASES
 =============================== */
