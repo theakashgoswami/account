ae66778566d6e6a4388d6b60d9a71ef0b07f74c5
theakashgoswami <founder@agtechscript.in>
2026-04-10 22:09:35 +0530

    css fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import App from './App';
-import '/index.css';  
+import './index.css';  
 import { CONFIG } from './config';
 
 // ✅ Handle unauthorized event (improved)
